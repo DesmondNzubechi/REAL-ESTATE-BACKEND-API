91a9885b5f167394ff1554525ec05e6a53170eeb
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-08-04 05:38:10 +0100

    worked on the comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -46,7 +46,7 @@ exports.createComment = catchAsync(async (req, res, next) => {
 
 })
 
-exports.getAllTheComment = catchAsync(async (req, res, next) => {
+exports.getAllTheBlogComment = catchAsync(async (req, res, next) => {
     
     const allComment = await Comment.find().populate("user").populate("blog");
 
@@ -59,3 +59,52 @@ exports.getAllTheComment = catchAsync(async (req, res, next) => {
         }
     })
 })
+
+exports.getABlogComment = catchAsync(async (req, res, next) => {
+
+    const { commentId } = req.params;
+
+    const blogComment = await Comment.findOne({ blog: commentId }).populate("blog");
+
+    res.status(200).json({
+        status: "success",
+        message: 'blog comment fetch successful',
+        data: {
+            comment: blogComment
+        }
+    })
+})
+
+    exports.deleteComment = catchAsync(async (req, res, next) => {
+
+        const { commentId } = req.params;
+        
+        const deleteComment = await Comment.findByIdAndDelete(commentId, req.body)
+
+        res.status(200).json({
+            status: 'success',
+            message: "comment deleted successfully",
+            data: null
+        })
+    })
+
+
+exports.updateComment = catchAsync(async (req, res, next) => {
+    
+    const { commentId } = req.params;
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId, req.body, { new: true });
+
+    if (!updatedComment) {
+        return next(new AppError("Comment does not exist", 400))
+    }
+
+    res.status(200).json({
+        status: "success",
+        message: "comment updated",
+        data: {
+            comment : updatedComment
+        }
+    })
+
+    })
--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createComment } = require("../controllers/commentController");
+const { createComment, getAllTheComment, deleteComment, updateComment } = require("../controllers/commentController");
 
 const router = express.Router();
 
@@ -7,5 +7,21 @@ const router = express.Router();
 router
     .route("/createComment")
     .post(createComment)
+
+    router
+    .route("/getAllComment")
+    .get(getAllTheComment)
+
+    router
+    .route("/getABlogComment/:commentId")
+    .get(getAllTheComment)
+
+    router
+    .route("/deleteComment/:commentId")
+    .delete(deleteComment)
+
+    router
+    .route("/updateComment:commentId")
+    .patch(updateComment)
 
 module.exports = router;
