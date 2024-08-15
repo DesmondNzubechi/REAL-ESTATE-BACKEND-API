674c993f2f8e4d9b812cd3f9f3041f9a90818b0c
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-08-16 00:47:08 +0100

    new change

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
  
 app.use(cors({
-    origin: 'https://home-features-backend.vercel.app',
+    origin: process.env.originUrl,
     credentials: true
 }))
 
